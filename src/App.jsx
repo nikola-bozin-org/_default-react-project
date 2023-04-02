364e9e55fed7b8aaf438a1bcf2e69f441b222b7b
nikola-bozin-org <nikola.bozin.org@gmail.com>
2023-04-02 08:46:56 +0200

    /

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import {BrowserRouter as Router, Route,Routes,Navigate} from 'react-router-dom'
 import Homepage from './pages/Homepage';
-import Test from './pages/Test';
-import E404 from './pages/E404';
 import { useEffect } from 'react';
 import Aos from 'aos';
 import "aos/dist/aos.css";
@@ -14,14 +11,7 @@ function App() {
   },[])
 
   return (
-    <Router>
-    <Routes>
-    <Route exact path="/" element={<Homepage/>}/>
-    <Route exact path="/test1/test2" element={<Test/>}/>
-    <Route exact path="/404" element={<E404/>}/>
-    <Route path="*" element={<E404/>}/>
-    </Routes>
-  </Router>
+    <Homepage/>
   );
 }
 
