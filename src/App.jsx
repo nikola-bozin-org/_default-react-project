ced30427398c5c2334d2cfb95f6147ef44c5b2a7
nikola-bozin-org <nikola.bozin.org@gmail.com>
2023-04-05 12:46:15 +0200

    /

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import Homepage from './pages/Homepage';
 import './app.css'
-
+import Aos from 'aos';
+import "aos/dist/aos.css";
+import React from 'react';
+import { useEffect } from 'react';
 function App() {
+  
+  useEffect(()=>{
+    Aos.init({duration:1000})
+  },[])
 
   return (
-    <div className='app'>
-      <Homepage/>
+    <div data-aos='fade-down' className='app'>
+      App
     </div>
   );
 }
